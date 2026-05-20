4c9a66ed5629888f7ed4aec6872159da7d91b9b4
jekkinoopy <jekkinoopy@gmail.com>
2026-05-20 16:17:18 +0800

    style: 辦案筆記改鑑識卷宗全文

--- courses/01-database/php/school/02-api_register.php
+++ courses/01-database/php/school/02-api_register.php
@@ -13,7 +13,12 @@ echo "</pre>";
 //發生了什麼： 伺服器一走進來，先幫你建立 $pdo 連線物件，然後立刻把前端送來的 $_POST 包裹拆開，啪地一聲印在網頁最畫面上。
 
 $sql="insert into `members`(`account`,`password`,`email`,`tel`,`birthday`)"
-            values('{$_POST[`account`]}',
-                    '{$_POST[`password`]}',
+            values('{$_POST['account']}',
+            //'{$_POST['account']}'第一層 SQL的文字桶
+            //{$_POST['account']}第二層 PHP的隔離罩
+            //['account']第三層 PHP的中括號標籤 專門寫給$_POST看
+                    '{$_POST['password']}',
+
+                
                     );
 ?>
